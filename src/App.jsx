55da28c3b0061778f6cc5743398c07595f09fc05
charlee <charleearodriguez@gmail.com>
2025-09-02 15:57:06 -0400

    Prepare for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
 import React from 'react'
-import {Routes,Route} from "react-router";
-import Navbar from "./assets/components/components/Navbar";
-import Home from "./routes/Home";
-import About from "./routes/About";
-import Contact from "./routes/Contact";
-import Portfolio from "./routes/Portfolio";
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./assets/components/components/Navbar"
+import Home from "./routes/Home"
+import About from "./routes/About"
+import Contact from "./routes/Contact"
+import Portfolio from "./routes/Portfolio"
 
 const App = () => {
   return (
-    
-    <div className = "min-h-screen flex flex-col bg-gray-200">
+    <div className="min-h-screen flex flex-col bg-gray-200">
       <Navbar />
       <Routes>
-        <Route path = "/" element = {<Home />} />
-        <Route path = "/about" element = {<About />} />
-        <Route path = "/portfolio" element = {<Portfolio />} />
-        <Route path = "/contact" element = {<Contact />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
-
     </div>
   )
 }
 
 export default App
-
