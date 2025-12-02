3ed7efac7ddefc77243dbf5c95d266314eadcd71
charleebanx <charleecodez@gmail.com>
2025-12-01 23:43:23 -0500

    Capstone Final Updates

--- src/assets/components/Cards/cardInfo.js
+++ src/assets/components/Cards/cardInfo.js
@@ -2,21 +2,21 @@ const baseLink = '/e-portfolio';
 export const capstoneInfo = [
   {
     id: 1,
-    image: "/images/projectmanagement.jpg",
+    image: "images/projectmanagement.jpg",
     title: "Project Management Plan",
     description: "Detailed essay outlining the scope, timeline, risks, and resources for a scenario as a project manager, providing structured guidance for project execution, milestones, and performance evaluation.",
     link: `${baseLink}/files/capstone/LIS4934%20Project%20Management%20Plan.pdf`
   },
   {
     id:2,
-    image: "/images/ethicalCase.jpg",
+    image: "images/ethicalCase.jpg",
     title: "Ethical Case Study: Artificial Intelligence",
     description: "Analytical report exploring ethical considerations and potential societal impacts of artificial intelligence implementation, addressing data privacy, fairness, and transparency in modern AI systems.",
     link: `${baseLink}/files/capstone/LIS4934 Ethical Case Study.pdf`
   },
   {
     id:3,
-    image: "/images/informationpolicy.jpg",
+    image: "images/informationpolicy.jpg",
     title: "Information Policy/Tech In News",
     description: "Research summary discussing current developments in information policy and technology news, evaluating regulations, technological advancements, and their implications on privacy, security, and innovation.",
     link: `${baseLink}/files/capstone/LIS4934%20Information%20Policy.pdf`
@@ -33,28 +33,28 @@ export const programmingInfo = [
   {
     id: 1,
     title: "GlideBin",
-    image: "/images/glidebin.png",
+    image: "images/glidebin.png",
     description: "File decluttering application that allows users to organize files efficiently, with features including file previews, path selection, and intuitive management tools to streamline digital workspace organization.",
     link: "https://github.com/charleebanx/glidebin"
   },
   {
     id: 2,
     title: "Employee Management API",
-    image: "/images/employeemanage.jpg",
+    image: "images/employeemanage.jpg",
     description: "Comprehensive API designed for managing employee data, including CRUD operations, authentication, and reporting endpoints, enabling seamless integration with front-end applications or internal dashboards.",
     link: "https://github.com/charleebanx/EmployeeManagementAPI"
   },
   {
     id: 3,
     title: "E-Portfolio",
-    image: "/images/eportfoliostock.jpg",
+    image: "images/eportfoliostock.jpg",
     description: "Personal e-portfolio website showcasing projects, experiences, and professional achievements, with responsive design, interactive elements, and intuitive navigation for user engagement.",
     link: "https://github.com/charleebanx/e-portfolio"
   },
   {
     id:4,
     title: "Homerun Probability Model",
-    image: "/images/homerunstock.jpg",
+    image: "images/homerunstock.jpg",
     description: "Data-driven statistical model to calculate the probability of home runs in baseball, utilizing historical data analysis, probability theory, and visualization techniques to support predictive analytics.",
     link: "https://github.com/charleebanx/homerunprobability"
   }
@@ -64,7 +64,7 @@ export const uiuxInfo = [
   {
     id: 1,
     title: "Replayr",
-    image: "/images/replayr.jpg",
+    image: "images/replayr.jpg",
     description: "UI/UX case study project for a new music streaming platform, detailing user research, wireframes, prototyping, and usability testing, with focus on intuitive navigation, visual aesthetics, and enhancing the overall user experience. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     link: `${baseLink}/files/mock-redesign.pdf`
   }
